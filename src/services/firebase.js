5c2fa1c5016dbc08eed1396856109575dfdcbf73
Rakhimbekov Rakhat <rkhantemir@mail.ru>
2025-03-12 16:21:45 +0500

    firebase and authentication added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,10 @@
 firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBB2K540j-GerRu7UoIKFZjfsMMRLYcfnk",
+  authDomain: "react-messenger-c3d72.firebaseapp.com",
+  projectId: "react-messenger-c3d72",
+  storageBucket: "react-messenger-c3d72.firebasestorage.app",
+  messagingSenderId: "652932615126",
+  appId: "1:652932615126:web:0136d8d2da5061ded03277"
 };
 
 const app = initializeApp(firebaseConfig);
